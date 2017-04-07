8ca2487adb4aed9f2edf7fff30af992ed203d69f
Martin Stoehr <Martin.Stoehr@uni.lu>
2017-04-07 15:42:41 +0200

    resolving final issues in current version of kMBD

--- ase/calculators/reciprocal_mbd.py
+++ ase/calculators/reciprocal_mbd.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-from mbd_scalapack import mbd_scalapack as mbd
+from mbd_scalapack import mbd_scalapack as mbd_s
 #from mbd import mbd
 from pymbd import get_free_atom_data, get_damping
 from ase import atoms
@@ -16,7 +16,7 @@ default_parameters = {'xc':'PBE',
                       'TS_cutoff':30./Bohr,
                       'ewald_real_cutoff_scaling':0.9,
                       'ewald_rec_cutoff_scaling':1.,
-                      'k_grid_shift':0.,
+                      'k_grid_shift':0.5,
                       'vacuum_axis':(True,True,True),
                       'max_nbody_MBD':3,
                       'do_reciprocal':True,
@@ -169,46 +169,43 @@ class kSpace_MBD_calculator(Calculator):
         """
         Initialize MBD calculation and evaluate properties.
         """
-        solver_bak = self.eigensolver
-        self.eigensolver = self.eigensolver.strip()[:5].ljust(5)
-        if self.eigensolver not in ['qr   ', 'mrrr ', 'dandc']:
-            print("The specified eigensolver '"+solver_bak.strip()+"' is not known (yet).")
-            print("Using default solver 'qr' instead...")
-            self.eigensolver = 'qr   '
+#        if (self.use_scalapack):
+        mbd_mod = mbd_s
+#        else:
+#            mbd_mod = mbd
         
-        mbd.init_grid(self.n_omega_SCS)
-        mbd.param_ts_energy_accuracy = self.TS_accuracy
-        mbd.param_ts_cutoff_radius = self.TS_cutoff
-        mbd.param_ewald_real_cutoff_scaling = self.ewald_real_cutoff_scaling
-        mbd.param_ewald_rec_cutoff_scaling = self.ewald_rec_cutoff_scaling
-        mbd.param_k_grid_shift = self.k_grid_shift
-        mbd.param_mbd_nbody_max = self.max_nbody_MBD
-        mbd.param_vacuum_axis = self.vacuum_axis
-        mbd.param_zero_negative_eigs = self.set_negative_eigenvalues_zero
-        mbd.eigensolver = self.eigensolver
-        mbd.my_task, mbd.n_tasks = self.myid, self.ntasks
+        mbd_mod.init_grid(self.n_omega_SCS)
+        mbd_mod.param_ts_energy_accuracy = self.TS_accuracy
+        mbd_mod.param_ts_cutoff_radius = self.TS_cutoff
+        mbd_mod.param_ewald_real_cutoff_scaling = self.ewald_real_cutoff_scaling
+        mbd_mod.param_ewald_rec_cutoff_scaling = self.ewald_rec_cutoff_scaling
+        mbd_mod.param_k_grid_shift = self.k_grid_shift
+        mbd_mod.param_mbd_nbody_max = self.max_nbody_MBD
+        mbd_mod.param_vacuum_axis = self.vacuum_axis
+        mbd_mod.param_zero_negative_eigs = self.set_negative_eigenvalues_zero
+        mbd_mod.my_task, mbd_mod.n_tasks = self.myid, self.ntasks
+        if self.use_scalapack:
+            solver_bak = self.eigensolver
+            self.eigensolver = self.eigensolver.strip()[:5].ljust(5)
+            if self.eigensolver not in ['qr   ', 'mrrr ', 'dandc']:
+                print("The specified eigensolver '"+solver_bak.strip()+"' is not known (yet).")
+                print("Using default solver 'qr' instead...")
+                self.eigensolver = 'qr   '
+
+            mbd_mod.eigensolver = self.eigensolver
         
         assert hasattr(self, 'a_div_a0'), \
         "Please provide rescaling to obtain initial dispersion parameters from accurate free atom reference data via 'set_rescaling(rescaling)'!"
         
+        self.modus = ''
+        if (self.ntasks > 1): self.modus += 'P'
         if np.any(self.vacuum_axis == atoms.pbc):
             print('Your specification for vacuum_axis is not in line with the boundary conditions of the atoms object!')
         
-        if np.all(self.vacuum_axis):
-            self.modus = ''
-        else:
-            self.modus = 'C'
+        if (not np.all(self.vacuum_axis)):
+            self.modus += 'C'
             if not (self.do_reciprocal or self.do_supercell):
                 raise ValueError("You chose periodic boundary condition via vacuum_axis, but did not specify how to handle it (do_reciprocal or do_supercell)!")
-        
-        if self.do_reciprocal:
-            self.modus.replace('C', 'R')
-        if (self.ntasks > 1):
-            self.modus += 'P'
-        if self.get_MBD_eigenvalues:
-            self.modus += 'E'
-        if self.get_MBD_eigenvectors:
-            self.modus += 'V'
         
         self.pos = atoms.positions/Bohr
         self.UC = atoms.get_cell()/Bohr
@@ -217,62 +214,73 @@ class kSpace_MBD_calculator(Calculator):
         self.alpha_0_TS = self.alpha_f*self.a_div_a0
         self.C6_TS = self.C6_f*self.a_div_a0*self.a_div_a0
         self.RvdW_TS = self.RvdW_f*self.a_div_a0**(1./3.)
-        self.omega_TS = mbd.omega_eff(self.C6_TS, self.alpha_0_TS)
-        
-        if ((self.do_TSSCS) and (not self.do_SCS)):
-            print('You requested TS energy with screened (SCS) polarizabilities,')
-            print("but didn't set do_SCS to True! I will correct this for you...")
-            self.do_SCS = True
+        self.omega_TS = mbd_mod.omega_eff(self.C6_TS, self.alpha_0_TS)
         
         self._get_SCS_and_MBD_damping_params()
         if self.do_SCS:
             self._run_electrostatic_screening()
         
-        self._get_mbd_energy()
-        if self.do_TS:
-            self._get_TS_energy1()
-        if self.do_TSSCS:
-            self._get_TSSCS_energy1()
+        if self.do_reciprocal: self.modus += 'R' #.replace('C','R')
+        if self.get_MBD_eigenvalues: self.modus += 'E'
+        if self.get_MBD_eigenvectors: self.modus += 'V'
         
-        mbd.destroy_grid()
-#        # finalize should be handled by mpi4py, I think...
-#        mbd.exit_blacs_and_finalize(1)
+        self._get_mbd_energy()
+        if self.do_TS: self._get_TS_energy1()
+        if self.do_TSSCS:
+            if not self.do_SCS:
+                print('You requested TS energy with screened (SCS) polarizabilities,')
+                print("but didn't set do_SCS to True. Skipping...")
+            else:
+                self._get_TSSCS_energy1()
+        
+        mbd_mod.destroy_grid()
+#       if self.use_scalapack:
+#            # finalize should be handled by mpi4py, I think...
+#            mbd_mod.exit_blacs_and_finalize(1)
         
     
     def _run_electrostatic_screening(self):
-        modus_scs = self.modus.replace('R','')
-        self.alpha_dyn_TS = mbd.alpha_dynamic_ts_all('C', \
-                                                     mbd.n_grid_omega, \
-                                                     self.alpha_0_TS, \
-                                                     c6=self.C6_TS)
+#        if (self.use_scalapack):
+        mbd_mod = mbd_s
+#        else:
+#            mbd_mod = mbd
+        
+        self.alpha_dyn_TS = mbd_mod.alpha_dynamic_ts_all('C', \
+                                                 mbd_mod.n_grid_omega, \
+                                                 self.alpha_0_TS, \
+                                                 c6=self.C6_TS)
         if self.use_scalapack: # use ScaLAPACK?
-            self.alpha_dyn_SCS = mbd.run_scs_s(modus_scs, \
-                                               self.Coulomb_SCS, \
-                                               self.pos, \
-                                               self.alpha_dyn_TS, \
-                                               r_vdw=self.RvdW_TS, \
-                                               beta=self.damp_par_beta, \
-                                               a=self.damp_par_a, \
-                                               unit_cell=self.UC)
+            self.alpha_dyn_SCS = mbd_mod.run_scs_s(self.modus, \
+                                              self.Coulomb_SCS, \
+                                              self.pos, \
+                                              self.alpha_dyn_TS, \
+                                              r_vdw=self.RvdW_TS, \
+                                              beta=self.damp_par_beta, \
+                                              a=self.damp_par_a, \
+                                              unit_cell=self.UC)
         else:
-            self.alpha_dyn_SCS = mbd.run_scs(modus_scs, \
-                                             self.Coulomb_SCS, \
-                                             self.pos, \
-                                             self.alpha_dyn_TS, \
-                                             r_vdw=self.RvdW_TS, \
-                                             beta=self.damp_par_beta, \
-                                             a=self.damp_par_a, \
-                                             unit_cell=self.UC)
+            self.alpha_dyn_SCS = mbd_mod.run_scs(self.modus, \
+                                            self.Coulomb_SCS, \
+                                            self.pos, \
+                                            self.alpha_dyn_TS, \
+                                            r_vdw=self.RvdW_TS, \
+                                            beta=self.damp_par_beta, \
+                                            a=self.damp_par_a, \
+                                            unit_cell=self.UC)
         
         self.alpha_0_SCS = self.alpha_dyn_SCS[0]
-        self.C6_SCS = mbd.get_c6_from_alpha(self.alpha_dyn_SCS)
+        self.C6_SCS = mbd_mod.get_c6_from_alpha(self.alpha_dyn_SCS)
         self.RvdW_SCS = self.RvdW_TS*(self.alpha_0_SCS/self.alpha_0_TS)**(1./3.)
-        self.omega_SCS = mbd.omega_eff(self.C6_SCS, self.alpha_0_SCS)
+        self.omega_SCS = mbd_mod.omega_eff(self.C6_SCS, self.alpha_0_SCS)
         
     
     def _get_mbd_energy(self):
-        g_grid = mbd.make_g_grid(self.Ggrid[0],self.Ggrid[1],self.Ggrid[2])
-        kgrid = mbd.make_k_grid(g_grid, self.UC)
+#        if (self.use_scalapack):
+        mbd_mod = mbd_s
+#        else:
+#            mbd_mod = mbd
+        
+        kgrid = mbd_mod.make_k_grid(mbd_mod.make_g_grid(*self.Ggrid), self.UC)
         
         if self.do_SCS:     # do MBD@(rs)SCS?
             alph, om, rvdwAB = self.alpha_0_SCS, self.omega_SCS, self.RvdW_SCS
@@ -280,33 +288,39 @@ class kSpace_MBD_calculator(Calculator):
             alph, om, rvdwAB = self.alpha_0_TS, self.omega_TS, self.RvdW_TS
         
         if self.use_scalapack:
-            self.E_MBD = mbd.get_mbd_energy_s(self.modus, \
-                                              self.Coulomb_CFDM, \
-                                              self.pos, alph, om, \
-                                              supercell=self.supercell, \
-                                              k_grid=kgrid, \
-                                              unit_cell=self.UC, \
-                                              r_vdw=rvdwAB, \
-                                              beta=self.damp_par_beta, \
-                                              a=self.damp_par_a)
+            self.E_MBD = mbd_mod.get_mbd_energy_s(self.modus, \
+                                             self.Coulomb_CFDM, \
+                                             self.pos, alph, om, \
+                                             supercell=self.supercell, \
+                                             k_grid=kgrid, \
+                                             unit_cell=self.UC, \
+                                             r_vdw=rvdwAB, \
+                                             beta=self.damp_par_beta, \
+                                             a=self.damp_par_a)
         else:
-            self.E_MBD = mbd.get_mbd_energy(self.modus, \
-                                            self.Coulomb_CFDM, \
-                                            self.pos, alph, om, \
-                                            supercell=self.supercell, \
-                                            k_grid=kgrid, \
-                                            unit_cell=self.UC, \
-                                            r_vdw=rvdwAB, \
-                                            beta=self.damp_par_beta, \
-                                            a=self.damp_par_a)
+            self.E_MBD = mbd_mod.get_mbd_energy(self.modus, \
+                                             self.Coulomb_CFDM, \
+                                             self.pos, alph, om, \
+                                             supercell=self.supercell, \
+                                             k_grid=kgrid, \
+                                             unit_cell=self.UC, \
+                                             r_vdw=rvdwAB, \
+                                             beta=self.damp_par_beta, \
+                                             a=self.damp_par_a)
         
         self.E_MBD *= Hartree
         
     
     def _get_TS_energy1(self):
         self._get_TS_damping_params()
+#        if (self.use_scalapack):
+        mbd_mod = mbd_s
+#        else:
+#            mbd_mod = mbd
+        
         if self.use_scalapack:
-            self.E_TS = mbd.get_ts_energy_lowmem(self.modus, 'fermi', \
+            self.E_TS = mbd_mod.get_ts_energy_lowmem(self.modus, \
+                                                 'fermi', \
                                                  self.pos, self.C6_TS, \
                                                  self.alpha_0_TS, \
                                                  r_vdw=self.RvdW_TS, \
@@ -314,35 +328,42 @@ class kSpace_MBD_calculator(Calculator):
                                                  d=self.ts_d, \
                                                  unit_cell=self.UC)
         else:
-            self.E_TS = mbd.get_ts_energy(self.modus, 'fermi', \
-                                          self.pos, self.C6_TS, \
-                                          self.alpha_0_TS, \
-                                          r_vdw=self.RvdW_TS, \
-                                          s_r=self.ts_s_r, \
-                                          d=self.ts_d, \
-                                          unit_cell=self.UC)
+            self.E_TS = mbd_mod.get_ts_energy(self.modus, 'fermi', \
+                                             self.pos, self.C6_TS, \
+                                             self.alpha_0_TS, \
+                                             r_vdw=self.RvdW_TS, \
+                                             s_r=self.ts_s_r, \
+                                             d=self.ts_d, \
+                                             unit_cell=self.UC)
         
         self.E_TS *= Hartree
         
     
     def _get_TSSCS_energy1(self):
         self._get_TS_damping_params()
+#        if (self.use_scalapack):
+        mbd_mod = mbd_s
+#        else:
+#            mbd_mod = mbd
+        
         if self.use_scalapack:
-            self.E_TS_SCS = mbd.get_ts_energy_lowmem(self.modus, 'fermi2', \
-                                                     self.pos, self.C6_SCS, \
-                                                     self.alpha_0_SCS, \
-                                                     r_vdw=self.RvdW_SCS, \
-                                                     s_r=self.ts_s_r, \
-                                                     d=self.ts_d, \
-                                                     unit_cell=self.UC)
+            self.E_TS_SCS = mbd_mod.get_ts_energy_lowmem(self.modus, \
+                                                'fermi2', \
+                                                self.pos, self.C6_SCS, \
+                                                self.alpha_0_SCS, \
+                                                r_vdw=self.RvdW_SCS, \
+                                                s_r=self.ts_s_r, \
+                                                d=self.ts_d, \
+                                                unit_cell=self.UC)
         else:
-            self.E_TS_SCS = mbd.get_ts_energy(self.modus, 'fermi2', \
-                                              self.pos, self.C6_SCS, \
-                                              self.alpha_0_SCS, \
-                                              r_vdw=self.RvdW_SCS, \
-                                              s_r=self.ts_s_r, \
-                                              d=self.ts_d, \
-                                              unit_cell=self.UC)
+            self.E_TS_SCS = mbd_mod.get_ts_energy(self.modus, \
+                                                'fermi2', \
+                                                self.pos, self.C6_SCS, \
+                                                self.alpha_0_SCS, \
+                                                r_vdw=self.RvdW_SCS, \
+                                                s_r=self.ts_s_r, \
+                                                d=self.ts_d, \
+                                                unit_cell=self.UC)
         
         self.E_TS_SCS *= Hartree
         
